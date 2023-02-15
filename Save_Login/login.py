54a60e0b6bd012849ff43276c0dfe863304a0f34
rishabh11336 <67859818+rishabh11336@users.noreply.github.com>
2023-02-15 11:44:34 +0530

    Update login.py

--- Save_Login/login.py
+++ Save_Login/login.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 options = Options()
-options.add_argument("--user-data-dir=C:\\Users\\Ashley\\Desktop\\UserData") #Directory of folder where login data will save
+options.add_argument("--user-data-dir=C:\\Users\\Admin\\Desktop\\UserData") #Directory of folder where login data will save
 options.page_load_strategy = 'normal'
 driver = webdriver.Chrome(options=options)
 driver.get("https://www.doamainame.com/")
